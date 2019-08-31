a36b42bbe1aa78c98aca0851807c334ad394aba3
piyush <piyush@piyushs-MacBook-Pro.local>
2019-08-31 11:35:35 +0200

    First commit

---
+++ src/test/java/com/piyushmittal/tdd/iphone/IphoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.piyushmittal.tdd.iphone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IphoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
